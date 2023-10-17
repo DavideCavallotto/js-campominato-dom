1a9c51cd55ece3acb9e3d869bfb72d7fe7782aa6
DavideCavallotto <davidecavallotto92@gmail.com>
2023-10-17 09:53:00 +0200

    fix indentation

--- bonus.js
+++ bonus.js
@@ -10,34 +10,31 @@ const btnPlayDOMElement = document.getElementById('play-btn');
 console.log(btnPlayDOMElement);
 
 // - recupero in counter dal DOM
-let counterDOMElement = document.querySelector('.counter')
-console.log(counterDOMElement)
-console.log(counterDOMElement,'punti')
+let counterDOMElement = document.querySelector('.counter');
+console.log(counterDOMElement);
+console.log(counterDOMElement,'punti');
 
-
-
-let counter = 0
-
+let counter = 0;
 
 btnPlayDOMElement.addEventListener('click', function(){    
     // - recupero gli elementi della select dal DOM 
     const difficultyDOMElement = document.getElementById('difficulty');
     const value = difficultyDOMElement.options[difficultyDOMElement.selectedIndex].value;
     console.log(value);
-    let numberOfCell 
+    let numberOfCell;
 
     if (value ==='easy') {
-        numberOfCell = 100
-        gridGenerator(numberOfCell, '')
+        numberOfCell = 100;
+        gridGenerator(numberOfCell, '');
         gridDOMElement.classList.add('border-easy');
 
     } else if (value === 'normal') {
-        numberOfCell = 81
-        gridGenerator(numberOfCell, 'medium-difficulty')
+        numberOfCell = 81;
+        gridGenerator(numberOfCell, 'medium-difficulty');
         gridDOMElement.classList.add('border-normal');
     } else if (value === 'hard') {
-        numberOfCell = 49
-        gridGenerator(numberOfCell, 'hard-difficulty')
+        numberOfCell = 49;
+        gridGenerator(numberOfCell, 'hard-difficulty');
         gridDOMElement.classList.add('border-hard');
     }   
         
@@ -48,24 +45,21 @@ btnPlayDOMElement.addEventListener('click', function(){
     const bombs = getArrayOfRandomIntBetween(1,numberOfCell,16);
     for (let i = 0; i < gridItemDOMElements.length; i++) {
         
-        const currentitemElements = gridItemDOMElements[i];
-
-        
-        
-                
+        const currentitemElements = gridItemDOMElements[i];       
+                        
         currentitemElements.addEventListener('click', function () {
-            const cellNumber = parseInt(currentitemElements.innerHTML)
+            const cellNumber = parseInt(currentitemElements.innerHTML);
             if (bombs.includes(cellNumber)) {
 
                 currentitemElements.classList.add('red-cell');                               
-                gridDOMElement.classList.add('end-game')
-                alert('Hai perso ' + (counterDOMElement.innerHTML = 'Hai totalizzato: ' + counter + ' Punti'))
+                gridDOMElement.classList.add('end-game');
+                alert('Hai perso ' + (counterDOMElement.innerHTML = 'Hai totalizzato: ' + counter + ' Punti'));
             } else {
 
                 currentitemElements.classList.add('cyan-cell');
-                counter++                
+                counter++;                
                 
-                counterDOMElement.innerHTML = 'Punti: ' + counter            
+                counterDOMElement.innerHTML = 'Punti: ' + counter;            
             }
 
             console.log('ho cliccato sulla casella ' + currentitemElements.innerHTML);                         
@@ -74,14 +68,14 @@ btnPlayDOMElement.addEventListener('click', function(){
 
     }    
 })
-
+// creo una funzione che mi generi la griglia
 function gridGenerator(numCells, classCell) {
     const gridDOMElement = document.querySelector('.grid-container');
     gridDOMElement.innerHTML = '';
     gridDOMElement.classList.remove('border-grid', 'border-normal', 'border-hard'); 
-    gridDOMElement.classList.remove('end-game')
-    counter = 0 
-    counterDOMElement.innerHTML = '<div class="counter"></div>'   
+    gridDOMElement.classList.remove('end-game');
+    counter = 0; 
+    counterDOMElement.innerHTML = '<div class="counter"></div>';   
       
 
     for (let i = 0; i < numCells; i++) {
@@ -97,19 +91,20 @@ function gridGenerator(numCells, classCell) {
 // const bombs = getArrayOfRandomIntBetween(1,100,16);
 // console.log(bombs)
 
-
+// creo una funzione che mi generi un tot di numeri 
 function getArrayOfRandomIntBetween(min,max,number) {
-    const bombsArray = []
+
+    const bombsArray = [];
 
     // popolare l'array con i numeri indicati in 'number'
     while(bombsArray.length < number) {
-        //generare numeri random che vanno da un minimo (min) ad un massimo (max)
-       const n = getRandomIntInclusive(min,max)
+       //generare numeri random che vanno da un minimo (min) ad un massimo (max)
+       const n = getRandomIntInclusive(min,max);
        //Se n non è presente nell'array
-       console.log(n)
+       console.log(n);
        if(bombsArray.includes(n) === false) {
            //pushare in numero nell'array
-            bombsArray.push(n)
+            bombsArray.push(n);
        } 
        // potremmo anche scrivere 
        //    if(!bombsArray.includes(n)) {
@@ -126,5 +121,5 @@ function getArrayOfRandomIntBetween(min,max,number) {
 function getRandomIntInclusive(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1) + min); // The maximum is inclusive and the minimum is inclusive
+    return Math.floor(Math.random() * (max - min + 1) + min); 
 }
